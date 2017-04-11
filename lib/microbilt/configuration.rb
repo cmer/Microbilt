572929d452a7980bf56abf23f3f02dcc6df09cba
Carl Mercier <carl@carlmercier.com>
2017-04-10 20:26:23 -0400

    Prettification

--- lib/microbilt/configuration.rb
+++ lib/microbilt/configuration.rb
@@ -1,15 +1,14 @@
 module Microbilt
   class Configuration
-    PROD_SERVER_URL  = 'https://creditserver.microbilt.com'
-    TEST_SERVER_URL  = 'https://sdkstage.microbilt.com'
+    PROD_SERVER_URL   = 'https://creditserver.microbilt.com'
+    TEST_SERVER_URL   = 'https://sdkstage.microbilt.com'
 
     CREATE_FORM_URI   = '/WebServices/IBV/Home/CreateForm'
     GET_DATA_URI      = '/WebServices/IBV/Home/GetData'
     GET_HTML_DATA_URI = '/WebServices/gethtml/gethtml.aspx'
+    ADD_CUSTOMER_URI  = '/WebServices/IBV/Home/AddCustomer'
 
-    ADD_CUSTOMER_URI = '/WebServices/IBV/Home/AddCustomer'
-
-    CONTENT_TYPE     = 'application/x-www-form-urlencoded'
+    CONTENT_TYPE      = 'application/x-www-form-urlencoded'
 
     attr_accessor :client_id, :client_password, :format, :server
 
